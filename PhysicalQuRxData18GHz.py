65887ba2d58ef74aa5c24d3e4b6c2ae70f9bf8e4
SoraOoshiro <e195740@ie.u-ryukyu.ac.jp>
2020-08-06 18:11:05 +0900

    (^^)v

---
+++ PhysicalQuRxData18GHz.py
@@ -0,0 +1,29 @@
+import csv
+import re
+import glob
+
+f_d_list = glob.glob("fixData/RxData/**", recursive=True)
+pattern_file = r'.*/192.168.100.9_csv.log$'
+file_name_list = []
+
+for sep in f_d_list:
+    res_file = re.match(pattern_file, sep)
+    if res_file:
+        file_name_list.append(sep)
+
+for file_name in file_name_list:
+    export_data_list = []
+    with open(file_name) as ori_file:
+        ori_data = csv.reader(ori_file)
+        for d in ori_data:
+            val = int(d[1])
+            if val < 0:
+                val += 256
+            val = int(0.5 * val) - 121
+            d[1] = str(val)
+            export_data_list.append(d)
+
+    with open(file_name, 'w') as exp_file:
+        exp_data = csv.writer(exp_file)
+        exp_data.writerows(export_data_list)
+
