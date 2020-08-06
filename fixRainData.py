00775f32deac29c990e0595a5756245512fa7f70
SoraOoshiro <e195740@ie.u-ryukyu.ac.jp>
2020-08-06 14:08:20 +0900

    v(^^)v

--- fixRainData.py
+++ fixRainData.py
@@ -1,18 +1,45 @@
 import csv
+import glob
+import re
+import pathlib
 
-file_path = 'RainData/20090616/20090616_rain.csv'
-export_data = []
+f_list = glob.glob("RainData/**", recursive=True)
+pattern_file = r'.*/[0-9]*_rain\.csv$'
+pattern_dir = r'RainData/[0-9]+$'
+f_name_list = []
+direc = []
 
-with open(file_path) as file:
-    reader = csv.reader(file)
-    cnt = 0
-    for r in reader:
-        if cnt < 2:
-            cnt += 1
-            continue
-        r[1] = str(int(r[1]) * 0.0083333 * 60)
-        export_data.append(r)
-        cnt += 1
+for sep in f_list:
+    res_file = re.match(pattern_file, sep)
+    res_dir = re.match(pattern_dir, sep)
+    if res_file:
+        f_name_list.append(sep)
+    if res_dir:
+        direc.append(sep)
 
-for i in export_data:
-    print(i)
+for di in direc:
+    di = 'fixData/'+di
+    p = pathlib.Path(di)
+    p.mkdir(parents=True)
+
+# 欠損csvファイル数
+loss_csv_file = len(direc)-len(f_name_list)
+for ori_file_name in f_name_list:
+    export_path = 'fixData/' + ori_file_name
+    export_data = []
+    with open(ori_file_name) as ori_file:
+        reader = csv.reader(ori_file)
+        cnt = 0
+        for r in reader:
+            if cnt < 2:
+                cnt += 1
+                continue
+            if len(r) != 2:
+                continue
+            r[1] = str(int(r[1]) * 0.0083333 * 60)
+            export_data.append(r)
+
+    with open(export_path, 'w') as exp_file:
+        writer = csv.writer(exp_file)
+        writer.writerows(export_data)
+
